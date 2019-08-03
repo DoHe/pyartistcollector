a64ffd496a9e338cc76de85aa67a3d1297e68bed
dohe <wahlholz@gmx.de>
2019-08-03 14:54:59 +0200

    Only add new albums

--- collect.py
+++ collect.py
@@ -1,6 +1,8 @@
+import pprint
 import json
 import os
 import sys
+import time
 
 from dotenv import load_dotenv
 from mutagen.easyid3 import EasyID3
@@ -9,10 +11,18 @@ from spotipy.util import prompt_for_user_token
 
 load_dotenv()
 
+
 MUSIC_EXTS = [
     ".mp3",
     ".wav",
     ".ogg"
+]
+
+IGNORED_ARTISTS = [
+    'various artists',
+    'unknown',
+    'untitled',
+    ''
 ]
 
 
@@ -25,7 +35,7 @@ def get_artists(path):
     try:
         id3 = EasyID3(path)
         artists = id3.get("artist", [])
-        return [artist.lower() for artist in artists]
+        return [artist.lower() for artist in artists if artist.lower not in IGNORED_ARTISTS]
     except Exception as e:
         print("Couldn't open {}: {}".format(path, e), file=sys.stderr)
         return []
@@ -67,18 +77,28 @@ class SpotifyClient:
         spotify_artists = self.spotify_client.search(artist, type="artist").get('artists', {}).get('items')
         if not spotify_artists:
             return
+        print("Following", spotify_artists[0].get('name'))
         artist_id = spotify_artists[0].get("id")
         self.spotify_client.user_follow_artists(ids=[artist_id])
         albums = self.spotify_client.artist_albums(artist_id, limit=50).get('items')
-        album_ids = set()
+        album_ids = []
         for album in albums:
             if album.get('album_type') != 'album':
                 continue
-            album_ids.add(album.get('id'))
+            album_ids.append(album.get('id'))
         if album_ids:
-            for chunk in chunkify(list(album_ids), 5):
+            print("Found", len(album_ids), "albums in total for", artist.title())
+            contained = self.spotify_client.current_user_saved_albums_contains(album_ids)
+            new_album_ids = []
+            for album_id, is_contained in zip(album_ids, contained):
+                if not is_contained:
+                    new_album_ids.append(album_id)
+            if not new_album_ids:
+                print("All of them already in your library")
+            print("Adding {} new albums for {}".format(len(new_album_ids), artist.title()))
+            for chunk in chunkify(list(new_album_ids), 50):
                 self.spotify_client.current_user_saved_albums_add(albums=chunk)
-            print("Added {} albums for {}".format(len(album_ids), artist.title()))
+            time.sleep(1)
         else:
             print("No albums for", artist)
 
@@ -93,4 +113,4 @@ if __name__ == "__main__":
     artists = collect_artists(args.dirs)
     print(("Found {} artists".format(len(artists))))
     c = SpotifyClient()
-    c.upload_artists(artists)
+    c.upload_artists(sorted(artists))
